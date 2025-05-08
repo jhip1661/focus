c34022c714e98713efec92c3aaf5ac296e8b9337
hjan <hj49.an@gmail.com>
2025-05-08 20:56:02 +0900

    2open

--- xls.py
+++ xls.py
@@ -9,7 +9,7 @@ import difflib
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from openai import OpenAI
+import openai
 
 # 🔐 환경 변수에서 JSON 문자열 읽고 줄바꿈 처리
 CREDENTIALS_JSON = os.getenv("GSHEET_CREDENTIALS_JSON", "").replace('\\n', '\n')
